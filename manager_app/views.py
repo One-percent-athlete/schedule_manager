c575d5d0a9b4be9518836bb70ee7087503d67857
One-percent-athlete <one.percent.athlete@gmail.com>
2025-03-31 17:43:41 +0900

    Update views

--- manager_app/views.py
+++ manager_app/views.py
@@ -324,3 +324,34 @@ def delete_report(request, report_id):
     else:
         messages.success(request, "ログインしてください。")
         return redirect("login_user")
+    
+@login_required(login_url='/login_user/')
+def export_csv(request):
+    latest_time = DailyReport.objects.latest("-date_created")
+    if latest_time:
+        date_time = now.date()
+        str_time = date_time.strftime('%Y/%m/%d')
+        f = "現場毎作業日報" + "_" + str_time + ".csv"
+        response = HttpResponse(content_type='text/csv; charset=utf-8-sig')
+        filename = urllib.parse.quote((f).encode('utf-8'))
+        response['Content-Disposition'] = f'attachment; filename*=UTF-8\'\'{filename}'
+    writer = csv.writer(response)
+    daily_report_list = DailyReport.objects.filter(date_created__contains=date_time)
+    if daily_report_list:
+        writer.writerow(["作成日", "作業日", "作成者", "現場名", "取引先", "建退共", "作業員", "人数", "シフト", "場所", "開始時間", "終了時間", "休憩時間", "走行距離数", "高速道路乗り", "高速道路降り", "高速支払い方法", "駐車場料金", "宿泊料金", "その他支払い", "建替人", "作業内容", "連絡事項"])
+        for report in daily_report_list:
+            attendees = []
+            for a in report.workers.all():
+                attendees.append(a.fullname)
+            if report.kentaikyo:
+                writer.writerow([report.date_created.strftime("%Y/%m/%d"), report.working_date, report.created_by, report.genba.name, report.genba.client, "有", ' '.join([a for a in attendees]), len(attendees), report.shift, report.genba.address, report.start_time, report.end_time, report.break_time, report.distance, report.highway_start, report.highway_end, report.highway_payment, report.parking, report.hotel, f"{report.other_payment} {report.other_payment_amount}", report.paid_by, report.daily_details, report.daily_note ])
+            else:
+                writer.writerow([report.date_created.strftime("%Y/%m/%d"), report.working_date, report.created_by, report.genba.name, report.genba.client, "無", ' '.join([a for a in attendees]), len(attendees), report.shift, report.genba.address, report.start_time, report.end_time, report.break_time, report.distance, report.highway_start, report.highway_end, report.highway_payment, report.parking, report.hotel, f"{report.other_payment} {report.other_payment_amount}", report.paid_by, report.daily_details, report.daily_note ])
+        return response
+    else:
+        messages.success(request, "データがありません。")
+        return redirect("report_list")
+
+def sauna(request):
+    year = now.year
+    return render(request, "sauna/sauna.html", {"year": year})
